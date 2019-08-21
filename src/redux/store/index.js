e4af14b07db239de4fcd0d36713cf81335e4415c
pourabbas <sara.poorabas@gmail.com>
2019-08-21 18:50:20 +0430

    middleware & thunk & redux dev tools

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,6 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import Reducer from '../reducers/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(Reducer);
+export const store = createStore(Reducer, composeWithDevTools(applyMiddleware(thunk)));
